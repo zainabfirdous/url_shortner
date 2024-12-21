d9c77fc37ddef3417a88b78bd63f80af1084f2b2
zainabfirdous <zainabfirdous786@gmail.com>
2024-12-22 01:18:40 +0530

    uninstalled rateLimiter

--- server/test/createUrl.test.js
+++ server/test/createUrl.test.js
@@ -1,6 +1,18 @@
-const createUrl = require("../../service/shortUrl")
+const {createUrl} = require('../controller/shortUrl')
+const {shortUrl} = require('../models/url'); 
 
-jest.mock("../../crud")
+jest.mock('../crud/shortUrl', () => ({
+    findOne: jest.fn(),
+    create: jest.fn(),
+}));
+
+jest.mock('nanoid', () => ({
+    nanoid: jest.fn(),
+}));
+
+jest.mock('../constant', () => ({
+    BASE_URL: 'http://localhost:3000',
+}));
 
 describe("creating a short-url", ()=>{
     beforeEach(()=>{
@@ -8,21 +20,26 @@ describe("creating a short-url", ()=>{
     });
 
     it("created url successfully", async()=>{
-        shortUrl.findAll.mockResolveValue([]);
+       findOne.mockResolvedValue(null);
         
         const newShortUrl = {id:1, originalUrl: 'example.com/link/deeplink', shorturl:'example.com/short'}
-        shortUrl.create.mockResolveValue(newShortUrl)
+        create.mockResolvedValue(newShortUrl)
 
-        const originalUrl = 'example/link/deeplink/deeperlink'
-        const response = await createUrl(originalUrl);
+        let req = {
+            body : {
+            longUrl:'example/link/deeplink/deeperlink',
+            customAlias: 'short'
+        }}
+    
+        const response = await createUrl(req);
 
-        expect(response.status).toBe(200);
-        expect(response.message).toBe("successfully created shortUrl");
+        expect(response).toBe('http://localhost:3000/short')
+        //expect(response.message).toBe("successfully created shortUrl");
     })
 
     it("longUrl already exists", async()=>{
         const existingShortUrl = {id:1, originalUrl: 'example.com/link/deeplink', shorturl:'example.com/short'}
-        shortUrl.findAll.mockResolveValue(existingShortUrl);
+        shortUrl.findAll.mockResolvedValue(existingShortUrl);
 
         const originalUrl = 'example/link/deeplink/deeperlink'
         const response = await createUrl(originalUrl)
